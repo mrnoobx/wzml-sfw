f5c8a9f396d1cf0108d074110ce075a27cd57b7e
mrnoobx <154804791+mrnoobx@users.noreply.github.com>
2025-04-21 14:36:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -102,8 +102,8 @@ RCLONE_SERVE_USER = ""
 RCLONE_SERVE_PASS = ""
 
 # JDownloader
-JD_EMAIL = ""
-JD_PASS = ""
+JD_EMAIL = "suchitamiki06@gmail.com"
+JD_PASS = "@Labani25"
 
 # Sabnzbd
 USENET_SERVERS = [
