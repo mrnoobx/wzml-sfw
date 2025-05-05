bb62467481e0443c503373cf45457ffcae6d4b93
mrnoobx <154804791+mrnoobx@users.noreply.github.com>
2025-05-05 07:52:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ TG_PROXY = (
 )  # {"scheme": ”socks5”, "hostname": ””, "port": 1234, "username": ”user”, "password": ”pass”}
 USER_SESSION_STRING = ""
 CMD_SUFFIX = ""
-AUTHORIZED_CHATS = "-1002180300815"
+AUTHORIZED_CHATS = "-1002646724326"
 SUDO_USERS = ""
 STATUS_LIMIT = 10
 DEFAULT_UPLOAD = "rc"
